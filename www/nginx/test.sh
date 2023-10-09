51f391558102c476e92a12fc4418ad7fd824e7d2
Barry McCormick <bmccormick@debianbeast.maildragon.com>
2023-10-09 12:52:23 -0700

    test 10

--- www/nginx/test.sh
+++ www/nginx/test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-curl http://127.0.0.1
+response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
 
-
+      # Check if the response code is 200 (OK)
+      - if [ "$response" -eq 200 ]; then
+          echo "Web server is responding with HTTP 200 OK.";
+        else
+          echo "Web server is not responding with HTTP 200 OK. Response code: $response";
+          exit 1; # Exit with an error status
+        fi
