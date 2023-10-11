5bc4db7a579b22dad356945934c69bbd837ff1df
Barry McCormick <bmccormick@debianbeast.maildragon.com>
2023-10-11 11:36:55 -0700

    nginx test 17

--- www/nginx/test.sh
+++ www/nginx/test.sh
@@ -1,11 +1,11 @@
 bastille template testrun ../../www/nginx
-
+echo "template applied"
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
 
-      # Check if the response code is 200 (OK)
-      - if [ "$response" -eq 200 ]; then
+# Check if the response code is 200 (OK)
+if [ "$response" -eq 200 ]; then
           echo "Web server is responding with HTTP 200 OK.";
-        else
-          echo "Web server is not responding with HTTP 200 OK. Response code: $response";
-          exit 1; # Exit with an error status
-        fi
+else
+   echo "Web server is not responding with HTTP 200 OK. Response code: $response";
+   exit 1; # Exit with an error status
+fi
