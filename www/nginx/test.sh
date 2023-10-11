f3802c522ae5e9526f2a380c3dd951198960978b
Barry McCormick <bmccormick@debianbeast.maildragon.com>
2023-10-11 12:30:30 -0700

    nginx test 18

--- www/nginx/test.sh
+++ www/nginx/test.sh
@@ -1,7 +1,10 @@
+echo "starting test script"
 bastille template testrun ../../www/nginx
 echo "template applied"
+pkg install curl
+echo "curl installed"
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
-
+echo "tested response"
 # Check if the response code is 200 (OK)
 if [ "$response" -eq 200 ]; then
           echo "Web server is responding with HTTP 200 OK.";
