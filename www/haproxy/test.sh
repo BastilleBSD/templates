a0376a45fd6411691eba4812c8ca4dfb3ed6f5d0
Barry McCormick <bmccormick@debianbeast.maildragon.com>
2023-10-11 18:49:15 -0700

    start of haproxy test 1

---
+++ www/haproxy/test.sh
@@ -0,0 +1,17 @@
+echo "starting test script"
+cd ../../
+pwd
+bastille template testrun www/haproxy
+echo "template applied"
+bastille rdr testrun tcp 80 80
+bastille rdr testrun list
+response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
+echo "tested response"
+echo "response is:" $response
+# Check if the response code is 200 (OK)
+if [ "$response" -eq 200 ]; then
+          echo "Web server is responding with HTTP 200 OK.";
+else
+   echo "Web server is not responding with HTTP 200 OK. Response code: $response";
+   exit 1; # Exit with an error status
+fi
