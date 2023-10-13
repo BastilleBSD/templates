3a1b84cb5ab476e123635bf1c2056fca538b5e5d
Barry McCormick <bmccormick@debianbeast.maildragon.com>
2023-10-12 19:05:20 -0700

    added service check

--- www/nginx/test.sh
+++ www/nginx/test.sh
@@ -10,10 +10,14 @@ ifconfig
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
 echo "tested response"
 echo "response is:" $response
-# Check if the response code is 200 (OK)
-if [ "$response" -eq 200 ]; then
-          echo "Web server is responding with HTTP 200 OK.";
+
+
+# Define the name of the service you want to check
+service_name="nginx"
+
+# Use the `service` command to check if the service is running
+if service $service_name status | grep -q "is running"; then
+  echo "$service_name is running."
 else
-   echo "Web server is not responding with HTTP 200 OK. Response code: $response";
-   exit 1; # Exit with an error status
+  echo "$service_name is not running."
 fi
